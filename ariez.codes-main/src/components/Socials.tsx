251879b6c96e85b0b26d6a0852b0d2d1b207387a
ariez <ttkeaton@outlook.com>
2023-05-03 20:58:01 +0100

    fr

---
+++ ariez.codes-main/src/components/Socials.tsx
@@ -0,0 +1,60 @@
+import {
+  FaGithub,
+  FaTwitter,
+  FaDiscord,
+  FaEnvelope,
+  FaMastodon,
+} from "react-icons/fa/index";
+
+const links = [
+  {
+    url: "https://github.com/ariezinc",
+    icon: FaGithub,
+    newTab: true,
+    rel: "noreferrer",
+  },
+  {
+    url: "https://twitter.com/bestdevelopr",
+    icon: FaTwitter,
+    newTab: true,
+    rel: "noreferrer",
+  },
+  {
+    url: "https://discord.com/users/952354740858134628",
+    icon: FaDiscord,
+    newTab: true,
+    rel: "noreferrer",
+  },
+  {
+    url: "https://tech.lgbt/@ariez",
+    icon: FaMastodon,
+    newTab: true,
+    rel: "me",
+  },
+  {
+    url: "mailto:ariez@zeusteam.dev",
+    icon: FaEnvelope,
+    newTab: false,
+    rel: "noreferrer",
+  },
+];
+
+export default function SocialGallery() {
+  return (
+    <>
+      <div className="inline-flex h-full space-x-4 flex-auto text-2xl">
+        {links.map((link) => (
+          <a
+            href={link.url}
+            className="transition-transform hover:scale-105"
+            target={link.newTab ? "_blank" : undefined}
+            rel={link.rel}
+            key={link.url}
+          >
+            <link.icon />
+          </a>
+        ))}
+      </div>
+    </>
+  );
+}
